6e4fe3f1feac1b42f4cea1ce5e77162fe9e68824
michael1342 <michaelanazodo2024@gmail.com>
2026-05-22 15:00:30 +0100

    i've combined our codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactDom from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 )
